4b936dec4ff156f989a6a4be9c9a73e8b542a3fa
s4nduni <wagachchigesanduni@gmail.com>
2022-01-19 21:17:37 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Name   : Docker Installer 
+
+# Author : W.S.Kaushalya
+
+# Date   : 01/19/2022
+
+# Description : This will install Docker engine in any Ubuntu based desktop.
+
+
+# Uninstall old versions
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+
+# Set up the repository
+
+sudo apt-get update
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+# Add Docker’s official GPG key
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+# Set up the stable repository
+
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# Install Docker Engine
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
+# Verify that Docker Engine is installed correctly by running the hello-world image.
+
+sudo docker run hello-world
