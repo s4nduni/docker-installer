0c3ebad0426399c9bb93c9c94ba589f89a498a8b
s4nduni <wagachchigesanduni@gmail.com>
2022-01-19 22:00:43 +0530

    fix

--- install.sh
+++ install.sh
@@ -47,3 +47,11 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 # Verify that Docker Engine is installed correctly by running the hello-world image.
 
 sudo docker run hello-world
+
+
+# install Docker compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
